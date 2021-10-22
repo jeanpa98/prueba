8858b9e1b32c45ac812e22c817e15083d94aafef
JEanpa98 <jeanpanamito@gmail.com>
2021-10-22 16:37:39 -0500

    prueba2

---
+++ prueba1/src/prueba1/Prueba1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner entrada = new Scanner(System.in);
+    String Materia = "";
+    
+    String [] nombres = {"jeanpa"};
+    System.out.println ("nombres\n%s");
+    }
+    
+}
